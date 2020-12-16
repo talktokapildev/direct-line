65abfbe65262d3cecc80f645add7ae15066f12ae
Kapil Dev <talktokapildev@gmail.com>
2020-12-16 00:48:27 +0000

    initial commit

---
+++ apps/faqs/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'direct-line-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'faqs';
+}
